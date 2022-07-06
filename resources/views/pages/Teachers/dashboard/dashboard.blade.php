bf010e2f4551cd3ae61dce3af253f0d645ae4099
Ali <aliosama8399@gmail.com>
2022-07-06 18:46:48 +0200

    student dashboard  59

--- resources/views/pages/Teachers/dashboard/dashboard.blade.php
+++ resources/views/pages/Teachers/dashboard/dashboard.blade.php
@@ -41,7 +41,6 @@ preloader -->
         <div class="page-title" >
             <div class="row">
                 <div class="col-sm-6" >
-                    <h4 class="mb-0" style="font-family: 'Cairo', sans-serif">مرحبا بك : {{auth()->user()->Name}}</h4>
                 </div><br><br>
                 <div class="col-sm-6">
                     <ol class="breadcrumb pt-0 pr-0 float-left float-sm-right">
